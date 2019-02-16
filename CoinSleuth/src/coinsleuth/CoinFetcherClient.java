9138e283b9c2dd581c4f1d44f6b24cfc32fea9b6
LAPTOP-LDED\Edward Conn <cardseller2@gmail.com>
2019-02-16 11:19:35 -0500

    Still trying to convert to coins. JSON file saved.

--- CoinSleuth/src/coinsleuth/CoinFetcherClient.java
+++ CoinSleuth/src/coinsleuth/CoinFetcherClient.java
@@ -3,41 +3,51 @@ package coinsleuth;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.io.Reader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import org.json.JSONObject;
+import org.json.JSONArray;
 
 /**
  *
  * @author Edward Conn
  */
-final class CoinFetcherClient {
+class CoinFetcherClient {
 
         final static String SERVERNAME = "https://min-api.cryptocompare.com/data";
         final static String APIKEY = "&api_key=1fe77f4b6080bbc249c389035283479" 
                 + "bdcfbe5551ef3ebd230ec00cd1951b2c8";
         
-        protected JSONObject getAllCoinsJSON(){
+        protected JSONArray getAllCoinsJSON(){
             final String tickerSymbols = "BTC,ETH,XRP,LTC,EOS,BCH,USDT," 
                     +  "TRX,XLM,BNB,BSV,ADA,XMR,MIOTA,DASH";
             final String ALLCOINOPTION = "/pricemultifull?fsyms=" 
                     + tickerSymbols +"&tsyms=USD";
-            return getURLRequest(SERVERNAME + ALLCOINOPTION + APIKEY);
+            StringBuilder coinsStringBuilder 
+                    =  getURLRequest(SERVERNAME + ALLCOINOPTION + APIKEY);
+            try{
+                PrintWriter pw = new PrintWriter("allCoinInfo.txt");
+                pw.print(coinsStringBuilder.toString());
+                pw.close();
+            }catch(IOException e){
+                System.err.print(e);
+            }
+            return new JSONArray(coinsStringBuilder);
         }
         
         //data: The unix timestamp of interest 
         
-        protected JSONObject getSingleCoinJSONHistoric(String Ticker, long date){
+        protected JSONArray getSingleCoinJSONHistoric(String Ticker, long date){
             if(date > 0 && Ticker.length() > 1){
                 String coinOption = "pricehistorical?fsym="+ Ticker 
                         + "&tsyms=USD&ts=" + date;
-                return getURLRequest(SERVERNAME + coinOption + APIKEY);
+                return new JSONArray(getURLRequest(SERVERNAME + coinOption + APIKEY));
             }
             return null;
         }
         
-        protected JSONObject getURLRequest(String urlString){
+        protected StringBuilder getURLRequest(String urlString){
             try {
                 URL url = new URL(urlString);
                 HttpURLConnection con = (HttpURLConnection) url.openConnection();
@@ -52,9 +62,9 @@ final class CoinFetcherClient {
                     streamReader = new InputStreamReader(con.getInputStream());
                 }
                 
-                JSONObject JObj = new JSONObject(copyInputStream(streamReader)); 
+                StringBuilder allCoinString = copyInputStream(streamReader); 
                 con.disconnect();
-                return JObj;
+                return allCoinString;
                 
             } catch (IOException e) {
                 System.err.println(e);
