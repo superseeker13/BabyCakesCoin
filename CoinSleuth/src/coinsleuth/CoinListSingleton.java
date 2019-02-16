e20089600e96680d684b8ce765fa3c63558f7976
LAPTOP-LDED\Edward Conn <cardseller2@gmail.com>
2019-02-16 10:55:30 -0500

    Trying to convert to coins.

--- CoinSleuth/src/coinsleuth/CoinListSingleton.java
+++ CoinSleuth/src/coinsleuth/CoinListSingleton.java
@@ -1,28 +1,35 @@
 package coinsleuth;
 
-import java.net.HttpURLConnection;
-import java.net.URL;
-
-import java.io.Reader;
-import java.io.InputStreamReader;
-import java.io.BufferedReader;
-import java.io.IOException;
+import org.json.JSONArray;
 
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Observer;
+import java.util.Observable;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  *
  * @author Edward Conn
  */
-public class CoinListSingleton {
+public class CoinListSingleton extends Observable {
 
     private static final CoinFetcherClient CFC = new CoinFetcherClient();
-    private static List<Observer> observersList = new LinkedList<Observer>();
+    private static List<Coin> coinList = new LinkedList<>();
 
     private CoinListSingleton() {
-
+        updateCoinList();
+        
+        ScheduledExecutorService exec = Executors.newSingleThreadScheduledExecutor();
+        exec.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                updateCoinList();
+            }
+        }, 0, 60, TimeUnit.SECONDS);
     }
 
     public static CoinListSingleton getInstance() {
@@ -30,66 +37,13 @@ public class CoinListSingleton {
     }
 
     private static class CoinListSingletonHolder {
-
         private static final CoinListSingleton INSTANCE = new CoinListSingleton();
     }
 
-    private static final class CoinFetcherClient {
+    private static List<Coin> updateCoinList() {
+        JSONArray coinArray = CFC.getAllCoinsJSON();
+        List<Coin> result = new LinkedList<>();
 
-        final static String SERVERNAME = "https://min-api.cryptocompare.com/data";
-        final static String APIKEY = "1fe77f4b6080bbc249c389035283479bdcfbe5551ef3ebd230ec00cd1951b2c8";
-
-        CoinFetcherClient() {
-            getCoinJSON(SERVERNAME + "/pricemultifull?fsyms=BTC&tsyms=USD");
-        }
-        
-        /*
-            @return StringBuffer of JSON from url.
-        */
-        StringBuffer getCoinJSON(String urlString){
-            try {
-                URL url = new URL(urlString);
-                HttpURLConnection con = (HttpURLConnection) url.openConnection();
-                con.setRequestMethod("GET");
-
-                int status = con.getResponseCode();
-                Reader streamReader;
-                
-                if (status > 299) {
-                    streamReader = new InputStreamReader(con.getErrorStream());
-                } else {
-                    streamReader = new InputStreamReader(con.getInputStream());
-                }
-                
-                StringBuffer strBuff = readHTTPInput(streamReader);
-                con.disconnect();
-                return strBuff;
-                
-            } catch (IOException e) {
-                System.err.println(e);
-            }
-            return null;
-        }
-        
-        /*
-            Copies the input stream from the url.
-        */
-        StringBuffer readHTTPInput(Reader streamReader){
-            try{
-                StringBuffer content;
-                try (BufferedReader in = new BufferedReader(streamReader)) {
-                    String inputLine;
-                    content = new StringBuffer();
-                    while ((inputLine = in.readLine()) != null) {
-                        content.append(inputLine);
-                    }
-                }
-                return content;
-            }catch(IOException e){
-                System.err.println("IOException: " + e);
-            }
-            return null;
-        }
-
+        return result;
     }
 }
