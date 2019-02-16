a438209ddd06681eb50eefdc84363d956ab1301f
LAPTOP-LDED\Edward Conn <cardseller2@gmail.com>
2019-02-16 11:35:17 -0500

    What I got so far.

--- CoinSleuth/src/coinsleuth/CoinFetcherClient.java
+++ CoinSleuth/src/coinsleuth/CoinFetcherClient.java
@@ -13,13 +13,13 @@ import org.json.JSONArray;
  *
  * @author Edward Conn
  */
-class CoinFetcherClient {
+public final class CoinFetcherClient {
 
         final static String SERVERNAME = "https://min-api.cryptocompare.com/data";
         final static String APIKEY = "&api_key=1fe77f4b6080bbc249c389035283479" 
                 + "bdcfbe5551ef3ebd230ec00cd1951b2c8";
         
-        protected JSONArray getAllCoinsJSON(){
+        public JSONArray getAllCoinsJSON(){
             final String tickerSymbols = "BTC,ETH,XRP,LTC,EOS,BCH,USDT," 
                     +  "TRX,XLM,BNB,BSV,ADA,XMR,MIOTA,DASH";
             final String ALLCOINOPTION = "/pricemultifull?fsyms=" 
@@ -38,7 +38,7 @@ class CoinFetcherClient {
         
         //data: The unix timestamp of interest 
         
-        protected JSONArray getSingleCoinJSONHistoric(String Ticker, long date){
+        public JSONArray getSingleCoinJSONHistoric(String Ticker, long date){
             if(date > 0 && Ticker.length() > 1){
                 String coinOption = "pricehistorical?fsym="+ Ticker 
                         + "&tsyms=USD&ts=" + date;
@@ -47,7 +47,7 @@ class CoinFetcherClient {
             return null;
         }
         
-        protected StringBuilder getURLRequest(String urlString){
+        private StringBuilder getURLRequest(String urlString){
             try {
                 URL url = new URL(urlString);
                 HttpURLConnection con = (HttpURLConnection) url.openConnection();
@@ -72,7 +72,7 @@ class CoinFetcherClient {
             return null;
         }
         
-        protected StringBuilder copyInputStream(Reader streamReader){
+        private StringBuilder copyInputStream(Reader streamReader){
             
             try{
                 StringBuilder content;
