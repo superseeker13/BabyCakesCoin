51872f6caaa5f591784df04ed8f9a044f2193356
LAPTOP-LDED\Edward Conn <cardseller2@gmail.com>
2019-02-20 15:13:27 -0500

    Abstract lists and fetchers.

---
+++ CoinSleuth/src/coinsleuth/CoinArticle.java
@@ -0,0 +1,20 @@
+package coinsleuth;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author Edward Conn
+ */
+public class CoinArticle {    
+    public CoinArticle() {
+    }
+
+    public CoinArticle(final JSONObject jsonObj) {
+
+    }
+
+    public JSONObject toJSON() {
+        return new JSONObject(String.format(""));
+    }
+}
+
