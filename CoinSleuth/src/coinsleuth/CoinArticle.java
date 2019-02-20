b4436ac51a95052cc14b76783d6a9d5aeb545c08
LAPTOP-LDED\Edward Conn <cardseller2@gmail.com>
2019-02-20 15:39:34 -0500

    Articles almost done

--- CoinSleuth/src/coinsleuth/CoinArticle.java
+++ CoinSleuth/src/coinsleuth/CoinArticle.java
@@ -5,7 +5,12 @@ import org.json.JSONObject;
  *
  * @author Edward Conn
  */
-public class CoinArticle {    
+public class CoinArticle {   
+    String Author;
+    String Title;
+    String Description;
+    String URL;
+        
     public CoinArticle() {
     }
 
